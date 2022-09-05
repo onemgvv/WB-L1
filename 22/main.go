551227dc75b89dcdd762761e391d7606443ce837
onemgvv <onemgvv@gmail.com>
2022-09-05 21:00:14 +0300

    22 create package bignum

--- 22/main.go
+++ 22/main.go
@@ -1,42 +1,11 @@
 package main
 
 import (
+	"bignum"
 	"fmt"
-	"math/big"
 )
 
-type BigNumberWorker interface {
-	Add() *big.Int
-	Substact() *big.Int
-	Multiple() *big.Int
-	Divide() *big.Int
-}
-
-type BigNumbers struct {
-	n1, n2 *big.Int
-}
-
-func (b *BigNumbers) Add() *big.Int {
-	return new(big.Int).Add(b.n1, b.n2)
-}
-
-func (b *BigNumbers) Substact() *big.Int {
-	return new(big.Int).Sub(b.n1, b.n2)
-}
-
-func (b *BigNumbers) Multiple() *big.Int {
-	return new(big.Int).Mul(b.n1, b.n2)
-}
-
-func (b *BigNumbers) Divide() *big.Int {
-	return new(big.Int).Div(b.n1, b.n2)
-}
-
-func NewBigNumbers(n1, n2 int64) BigNumberWorker {
-	return &BigNumbers{big.NewInt(n1), big.NewInt(n2)}	
-}
-
 func main() {
-	bNumber := NewBigNumbers(724389233415407581, 292301003375467513)
+	bNumber := bignum.NewBigNumbers(724389233415407581, 292301003375467513)
 	fmt.Printf("Add: %d\nSubstract: %d\nMultiple: %d\nDivide: %d\n", bNumber.Add(), bNumber.Substact(), bNumber.Multiple(), bNumber.Divide())
 }
